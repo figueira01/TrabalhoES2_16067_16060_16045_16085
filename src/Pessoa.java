0b3d1d879a5682d966c82bcd7b09232cfc63454b
figueira01 <leinad.sf@hotmail.com>
2019-05-16 15:07:44 +0100

    First commit

---
+++ src/Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private String sexo;
+    private String profissao;
+
+    public Pessoa(String nome, int idade, String sexo, String profissao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.profissao = profissao;
+    }
+}
