505a66e44ef082aec002ce42f9e272671fe26eda
ricar <ricardobenfiquista13@gmail.com>
2019-05-17 10:10:06 +0100

    ssss

---
+++ testsPlanoAlimentacao/PessoaTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PessoaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testConstrutorPessoa()
+    {
+        Pessoa p = new Pessoa("Bruno Miguel",20,"Masculino","Mestre da Informatica");
+    }
+}
