264fe1af7c040d8a539d6c08441969d23d99cc4c
ricar <ricardobenfiquista13@gmail.com>
2019-05-17 10:43:57 +0100

    ssss

--- testsPlanoAlimentar/TestPlanoAlimentar.java
+++ testsPlanoAlimentar/TestPlanoAlimentar.java
@@ -2,9 +2,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestPlanoAlimentar {
+
+    private LocalDate time;
 
     @BeforeEach
     void setUp() {
@@ -17,11 +21,18 @@ class TestPlanoAlimentar {
     @Test
     void testConstrutorPessoa()
     {
-        Pessoa p = new Pessoa("Bruno Miguel",20,"Masculino","Mestre da Informatica");
+        Pessoa p = new Pessoa("Bruno Miguel",20,"Masculino","Mestre da Programcao");
     }
+
     @Test
     void testContrutorDadosFisicos()
     {
-        //DadosFisicos dF = new DadosFisicos(65,176,);
+        DadosFisicos dF = new DadosFisicos(65,1.76,"15%","11%","35%","21","11","11");
+    }
+
+    @Test
+    void testConstrutorHabitosAlimentares()
+    {
+        //HabitosAlimentares habitosAlimentares = new HabitosAlimentares("Almoco","arroz, Carne",);
     }
 }
