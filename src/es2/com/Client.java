6060815426fa5ed1cbf307c20b44999c40b210c0
figueira01 <leinad.sf@hotmail.com>
2019-05-23 15:52:10 +0100

    First commit

--- src/es2/com/Client.java
+++ src/es2/com/Client.java
@@ -1,8 +1,33 @@
 package es2.com;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Client {
+
+    private static final String COMMA_DELIMITER = ";";
 
     public static void main(String [] args)
     {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/leina/OneDrive/Ambiente de Trabalho/Faculdade/ES2/Trabalho ES2/Livro1.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                records.add(Arrays.asList(values));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(int i=0;i<records.size();i++)
+        {
+            System.out.println(records.get(i));
+        }
+
     }
 }
