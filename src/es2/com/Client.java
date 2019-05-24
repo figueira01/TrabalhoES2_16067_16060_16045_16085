c474347d73561cce81c68a27351b0bb0b24df07e
figueira01 <leinad.sf@hotmail.com>
2019-05-24 10:23:14 +0100

    First commit

--- src/es2/com/Client.java
+++ src/es2/com/Client.java
@@ -1,6 +1,7 @@
 package es2.com;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
--- src/es2/com/DadosFisicos.java
+++ src/es2/com/DadosFisicos.java
@@ -9,101 +9,111 @@ public class DadosFisicos extends Object{
     private String idadeMetabolica;
     private String osso;
     private Pessoa p;
+    private double imc;
+    private double musculo;
+    private double metabolismoBasal;
+    private double h20;
 
-    public DadosFisicos(double peso, double altura, double gorduraCorporal, double gorduraVisceral,String idadeMetabolica, String osso) {
+
+    public DadosFisicos(double peso, double altura, double gorduraCorporal, double gorduraVisceral, String idadeMetabolica, String osso, Pessoa p, double imc, double musculo, double metabolismoBasal, double h20) {
         this.peso = peso;
         this.altura = altura;
         this.gorduraCorporal = gorduraCorporal;
         this.gorduraVisceral = gorduraVisceral;
         this.idadeMetabolica = idadeMetabolica;
         this.osso = osso;
+        this.p = p;
+        this.imc = imc;
+        this.musculo = musculo;
+        this.metabolismoBasal = metabolismoBasal;
+        this.h20 = h20;
     }
 
     public double getPeso() {
         return peso;
     }
 
-    public double getAltura() {
-        return altura;
-    }
-
-    public double getGorduraCorporal() {return gorduraCorporal; }
-
-    public double getGorduraVisceral() {
-        return gorduraVisceral;
-    }
-
-    public String getIdadeMetabolica() {
-        return idadeMetabolica;
-    }
-
-
-    public String getOsso() {
-        return osso;
-    }
-
     public void setPeso(double peso) {
         this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
     }
 
     public void setAltura(double altura) {
         this.altura = altura;
     }
 
+    public double getGorduraCorporal() {
+        return gorduraCorporal;
+    }
+
     public void setGorduraCorporal(double gorduraCorporal) {
         this.gorduraCorporal = gorduraCorporal;
+    }
+
+    public double getGorduraVisceral() {
+        return gorduraVisceral;
     }
 
     public void setGorduraVisceral(double gorduraVisceral) {
         this.gorduraVisceral = gorduraVisceral;
     }
 
+    public String getIdadeMetabolica() {
+        return idadeMetabolica;
+    }
+
     public void setIdadeMetabolica(String idadeMetabolica) {
         this.idadeMetabolica = idadeMetabolica;
     }
 
+    public String getOsso() {
+        return osso;
+    }
 
     public void setOsso(String osso) {
         this.osso = osso;
     }
 
-    public double calculateIMC()
-    {
-        return peso/(altura*altura);
+    public Pessoa getP() {
+        return p;
     }
 
-    public double calculateMetabolismoBasal(float taxa){
-
-        if(p.getSexo().equals("Masculino"))
-        {
-            return taxa * (66 + ((13.7 * peso) + ( 5 * altura) - (6.8 * p.getIdade())));
-        }
-        else if(p.getSexo().equals("Feminino"))
-        {
-            return taxa * (655 + ((9.6 * peso) + ( 1.8 * altura) - (4.7 * p.getIdade())));
-        }
-        else
-            return 0;
+    public void setP(Pessoa p) {
+        this.p = p;
     }
 
-    public double calculateMusculo(){
-
-        return peso-(peso*gorduraCorporal);
+    public double getImc() {
+        return imc;
     }
 
-    public double calculateWater(){
-
-        if(p.getSexo().equals("Masculino"))
-        {
-            return 2.447 - 0.09156 * p.getIdade() + (altura*100) + 0.3362 * peso;
-        }
-        else if(p.getSexo().equals("Feminino"))
-        {
-            return -2.097 + 0.1069 * (altura*100) + 0.2466 * peso;
-        }
-        else
-            return 0;
+    public void setImc(double imc) {
+        this.imc = imc;
     }
 
+    public double getMusculo() {
+        return musculo;
+    }
 
+    public void setMusculo(double musculo) {
+        this.musculo = musculo;
+    }
+
+    public double getMetabolismoBasal() {
+        return metabolismoBasal;
+    }
+
+    public void setMetabolismoBasal(double metabolismoBasal) {
+        this.metabolismoBasal = metabolismoBasal;
+    }
+
+    public double getH20() {
+        return h20;
+    }
+
+    public void setH20(double h20) {
+        this.h20 = h20;
+    }
 }
