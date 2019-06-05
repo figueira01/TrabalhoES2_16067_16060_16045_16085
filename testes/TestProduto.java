8edf7ad4b8e28bae6d80134d1efd0238a50f15bc
Jorge <gandarelaisthebest@gmail.com>
2019-06-05 12:04:05 +0100

    teste

---
+++ testes/TestProduto.java
@@ -0,0 +1,19 @@
+import es2.com.Produto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestProduto
+{
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+
+}
