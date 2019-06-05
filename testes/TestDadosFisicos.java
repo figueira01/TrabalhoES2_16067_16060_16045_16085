08b93830698b7967e6004fe77cc0252bac91f3b3
Jorge <gandarelaisthebest@gmail.com>
2019-06-05 19:00:53 +0100

    teste Produto

--- testes/TestDadosFisicos.java
+++ testes/TestDadosFisicos.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestDadosFisicos
 {
-    private DadosFisicos  dadosFisicos;
+    private DadosFisicos dadosFisicos;
 
 
     @BeforeEach
     void setUp()
     {
-        DadosFisicos dadosFisicos = new DadosFisicos(70,178,5,30,23,3,
+        dadosFisicos = new DadosFisicos(70,178,20,30,23,3,
                 22,60,6500,65);
     }
 
@@ -26,14 +26,127 @@ public class TestDadosFisicos
     @Test
     void testeDadosFisicos()
     {
-        DadosFisicos dadosFisicos = new DadosFisicos(70,178,5,30,23,3,
+        dadosFisicos = new DadosFisicos(70,178,20,30,23,3,
                 22,60,6500,65);
     }
 
     @Test
-    void testepeso()
+    void testePeso()
     {
-        System.out.println("aqui!!");
         assertTrue( dadosFisicos.getPeso() >= 1 &&  dadosFisicos.getPeso() <= 300);
+    }
+
+    @Test
+    void testePesoFalse()
+    {
+        assertFalse( dadosFisicos.getPeso() < 1 ||  dadosFisicos.getPeso() > 300);
+    }
+
+    @Test
+    void testeAltura()
+    {
+        assertTrue( dadosFisicos.getAltura() >= 54 &&  dadosFisicos.getAltura() <= 250);
+    }
+
+    @Test
+    void testeAlturaFalse()
+    {
+        assertFalse( dadosFisicos.getAltura() < 54 ||  dadosFisicos.getAltura() > 250);
+    }
+
+    @Test
+    void testeGorduraCorporal()
+    {
+        assertTrue( dadosFisicos.getGorduraCorporal() >= 0 &&  dadosFisicos.getGorduraCorporal() <= 100);
+    }
+
+    @Test
+    void testeGorduraCorporalFalse()
+    {
+        assertFalse( dadosFisicos.getGorduraCorporal() < 0 ||  dadosFisicos.getGorduraCorporal() > 100);
+    }
+
+    @Test
+    void testeGorduraVisceral()
+    {
+        assertTrue( dadosFisicos.getGorduraVisceral() >= 0 &&  dadosFisicos.getGorduraVisceral() <= 50);
+    }
+
+    @Test
+    void testeGorduraVisceralFalse()
+    {
+        assertFalse( dadosFisicos.getGorduraVisceral() < 0 ||  dadosFisicos.getGorduraVisceral() > 50);
+    }
+
+    @Test
+    void testeIdadeMetabolica()
+    {
+        assertTrue(dadosFisicos.getIdadeMetabolica() >= 0 &&  dadosFisicos.getGorduraVisceral() <= 100);
+    }
+
+    @Test
+    void testeIdadeMetabolicaFalse()
+    {
+        assertFalse(dadosFisicos.getIdadeMetabolica()<  0 ||  dadosFisicos.getGorduraVisceral() > 100);
+    }
+
+    @Test
+    void testeOsso()
+    {
+        assertTrue(dadosFisicos.getOsso() >= 0 &&  dadosFisicos.getOsso() <= 5);
+    }
+
+    @Test
+    void testeOssoFalse()
+    {
+        assertFalse(dadosFisicos.getOsso() < 0 ||  dadosFisicos.getOsso() > 5);
+    }
+
+    @Test
+    void testeIMC()
+    {
+        assertTrue(dadosFisicos.getImc()>= 0 &&  dadosFisicos.getImc() <= 50);
+    }
+
+    @Test
+    void testeIMCFalse()
+    {
+        assertFalse(dadosFisicos.getImc() < 0 ||  dadosFisicos.getImc() > 50);
+    }
+
+    @Test
+    void testeMusculo()
+    {
+        assertTrue(dadosFisicos.getMusculo()>= 0 &&  dadosFisicos.getMusculo() <= 100);
+    }
+
+    @Test
+    void testeMusculoFalse()
+    {
+        assertFalse(dadosFisicos.getMusculo() < 0 &&  dadosFisicos.getMusculo() > 100);
+    }
+
+    @Test
+    void testeMetabolismoBasal()
+    {
+        assertTrue(dadosFisicos.getMetabolismoBasal()>= 0 &&  dadosFisicos.getMetabolismoBasal() <= 12000);
+    }
+
+    @Test
+    void testeMetabolismoBasalFalse()
+    {
+        assertFalse(dadosFisicos.getMetabolismoBasal() < 0 ||  dadosFisicos.getMetabolismoBasal() > 12000);
+    }
+
+    @Test
+    void testeH2O()
+    {
+        assertTrue(dadosFisicos.getH20()>= 0 &&  dadosFisicos.getH20() <= 100);
+    }
+
+    @Test
+    void testeH2OFalse()
+    {
+        assertFalse(dadosFisicos.getH20() < 0 ||  dadosFisicos.getH20() > 100);
     }
 }
