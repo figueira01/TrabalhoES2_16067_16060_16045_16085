b32c69c0ad8cd864d579e70a57b8ec9e2dcab47f
ricar <ricardobenfiquista13@gmail.com>
2019-06-19 10:54:31 +0100

    Merge branch 'Bruno' of https://github.com/figueira01/TrabalhoES2_16067_16060_16045_16085
    # Conflicts:
    #	TrabalhoFinalES2.iml
    #	src/es2/com/Client.java
    #	testes/TestPlanoAlimentar.java

---
+++ testes/TestQuestionario.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestQuestionario {
+
+    @BeforeEach
+    void setUp()
+    {
+
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+
+    }
+
+    @Test
+    void testContrutorPessoa()
+    {
+
+    }
+}
