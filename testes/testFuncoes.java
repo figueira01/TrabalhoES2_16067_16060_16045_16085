0d88210d57e62ac2c87c9d6cab1036c87ee3a7d1
ricar <ricardobenfiquista13@gmail.com>
2019-07-13 16:30:01 +0100

    Merge branch 'Bruno' of https://github.com/figueira01/TrabalhoES2_16067_16060_16045_16085
    # Conflicts:
    #	TrabalhoFinalES2.iml
    #	src/es2/com/Client.java
    #	testes/TestPlanoAlimentar.java

--- testes/testFuncoes.java
+++ testes/testFuncoes.java
@@ -1,4 +1,5 @@
 import es2.com.Client;
+import es2.com.Funcoes;
 import es2.com.PlanoAlimentar;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,8 +14,9 @@ public class testFuncoes {
 
     private List<List<PlanoAlimentar>> planoAtual;
     private String refeicao;
-    boolean bool;
-    private Client client;
+    private boolean bool;
+    private Funcoes funcoes;
+    private String url = "CSV/PlanoAlimentarAtual.csv";
 
     @BeforeEach
     void setUp()
@@ -22,8 +24,8 @@ public class testFuncoes {
         planoAtual = new ArrayList<>();
         refeicao = "pequeno almoco";
         bool = true;
-        client = new Client();
-        //client.LerPlanoAlimentar("CSV/PlanoAlimentarPrescrito.csv",planoAtual);
+        funcoes = new Funcoes();
+        //funcoes.LerPlanoAlimentar(url,);
     }
 
     @AfterEach
@@ -35,9 +37,6 @@ public class testFuncoes {
     @Test
     public void CalculationgetAcidoGordosSaturadosV()
     {
-
-        //double a = client.CalculationAcidoColesterol(planoAtual,refeicao,false);
-        //System.out.println(a);
-        //assertEquals(12.999,client.CalculationgetAcidoGordosSaturados(planoAtual,refeicao,bool));
+        //funcoes.CalculationgetAcidoGordosSaturados()
     }
 }
