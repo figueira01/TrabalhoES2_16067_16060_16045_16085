281ed5e7002d7e9764b00edfa7680df7fec74e2e
Daniel Figueira <leinad.sf@hotmail.com>
2019-07-14 01:36:06 +0100

    Merge branch 'master' of https://github.com/figueira01/TrabalhoES2_16067_16060_16045_16085
    # Conflicts:
    #	src/es2/com/Funcoes.java

--- src/es2/com/Produto.java
+++ src/es2/com/Produto.java
@@ -2,6 +2,7 @@ package es2.com;
 
 public class Produto extends Object{
 
+
     private String codigo;
     private String nome;
     private double energiaKcal;
@@ -47,6 +48,49 @@ public class Produto extends Object{
     private double Zn;
 
     public Produto(String codigo, String nome, double energiaKcal, double energiaKj, double agua, double proteina, double gorduraTotal, double totalHCDisponivel, double totalHCexpressoEmMonossacaridos, double monoPlusDissacaridos, double acidosOrganicos, double alcool, double amido, double oligossacaridos, double fibraAlimentar, double acidoGordosSaturados, double acidosGordosMonoinsaturados, double acidosGordosPolinsaturados, double acidosGordosTrans, double acidoLinoleico, double colesterol, double retonol,  double vitATotal, double caroteno, double vitD, double tocoferol, double tiamina, double riboflavina, double equivalentesDeNiacina, double niaciana, double triptofano60, double vitB6, double vitB12, double vitC, double folatos, double cinza, double na, double k, double ca, double p, double mg, double fe, double zn) {
+
+        assert codigo != null && codigo != "" : "Codigo nao pode ser null";
+        assert nome != null && nome != "" : "Nome nao pode ser null";
+        assert energiaKcal > 0 && energiaKcal < 500 : "Energia Kcal não pode ser menor que 0 e maior que 500";
+        assert energiaKj > 0 && energiaKj < 500 : "Energia Kj não pode ser menor que 0 e maior que 500";
+        assert agua > 0 && agua < 500 : "Agua não pode ser menor que 0 e maior que 500";
+        assert proteina > 0 && proteina < 500 : "Proteina não pode ser menor que 0 e maior que 500";
+        assert gorduraTotal > 0 && gorduraTotal < 500 : "Gordura Total não pode ser menor que 0 e maior que 500";
+        assert totalHCDisponivel > 0 && totalHCDisponivel < 500 : "Total HCD Disponivel não pode ser menor que 0 e maior que 500";
+        assert totalHCexpressoEmMonossacaridos > 0 && totalHCexpressoEmMonossacaridos < 500 : "Total HC Expressao em Monossacaridos não pode ser menor que 0 e maior que 500";
+        assert monoPlusDissacaridos > 0 && monoPlusDissacaridos < 500 : "Monoplus Dissacaridos não pode ser menor que 0 e maior que 500";
+        assert alcool > 0 && alcool < 500 : "Alcool não pode ser menor que 0 e maior que 500";
+        assert amido > 0 && amido < 500 : "Amido não pode ser menor que 0 e maior que 500";
+        assert oligossacaridos > 0 && oligossacaridos < 500 : "Oligossacaridos não pode ser menor que 0 e maior que 500";
+        assert fibraAlimentar > 0 && fibraAlimentar < 500 : "Fibra alimentar não pode ser menor que 0 e maior que 500";
+        assert acidoGordosSaturados > 0 && acidoGordosSaturados < 500 : "Acido Gordos Saturados não pode ser menor que 0 e maior que 500";
+        assert acidosGordosMonoinsaturados > 0 && acidosGordosMonoinsaturados < 500 : "Acidos Gordos Monoinsaturados não pode ser menor que 0 e maior que 500";
+        assert acidosGordosPolinsaturados > 0 && acidosGordosPolinsaturados < 500 : "Acidos Gordos Polinsaturados não pode ser menor que 0 e maior que 500";
+        assert acidosGordosTrans > 0 && acidosGordosTrans < 500 : "Acidos Gordos Trans não pode ser menor que 0 e maior que 500";
+        assert retonol > 0 && retonol < 500 : "Retonol não pode ser menor que 0 e maior que 500";
+        assert acidoLinoleico > 0 && acidoLinoleico < 500 : "Acido Linoleico não pode ser menor que 0 e maior que 500";
+        assert colesterol > 0 && colesterol < 500 : "Colesterol não pode ser menor que 0 e maior que 500";
+        assert vitATotal > 0 && vitATotal < 500 : "Vitamina A Total não pode ser menor que 0 e maior que 500";
+        assert caroteno > 0 && caroteno < 500 : "Caroteno não pode ser menor que 0 e maior que 500";
+        assert vitD > 0 && vitD < 500 : "Vitamina D não pode ser menor que 0 e maior que 500";
+        assert tocoferol > 0 && tocoferol < 500 : "Tocoferol não pode ser menor que 0 e maior que 500";
+        assert tiamina > 0 && tiamina < 500 : "Tiamina não pode ser menor que 0 e maior que 500";
+        assert equivalentesDeNiacina > 0 && equivalentesDeNiacina < 500 : "Equivalentes de Niacina não pode ser menor que 0 e maior que 500";
+        assert niaciana > 0 && niaciana < 500 : "Niaciana não pode ser menor que 0 e maior que 500";
+        assert triptofano60 > 0 && triptofano60 < 500 : "Triptofano60 não pode ser menor que 0 e maior que 500";
+        assert vitB6 > 0 && vitB6 < 500 : "Vitamina B6 não pode ser menor que 0 e maior que 500";
+        assert vitB12 > 0 && vitB12 < 500 : "Vitamina B12 não pode ser menor que 0 e maior que 500";
+        assert vitC > 0 && vitC < 500 : "Niaciana não pode ser menor que 0 e maior que 500";
+        assert folatos > 0 && folatos < 500 : "Folatos não pode ser menor que 0 e maior que 500";
+        assert cinza > 0 && cinza < 500 : "Cinza não pode ser menor que 0 e maior que 500";
+        assert na > 0 && na < 500 : "Na não pode ser menor que 0 e maior que 500";
+        assert k > 0 && k < 500 : "k não pode ser menor que 0 e maior que 500";
+        assert ca > 0 && ca < 500 : "Ca não pode ser menor que 0 e maior que 500";
+        assert p > 0 && p < 500 : "P não pode ser menor que 0 e maior que 500";
+        assert mg > 0 && mg < 500 : "Mg não pode ser menor que 0 e maior que 500";
+        assert fe > 0 && fe < 500 : "Fe não pode ser menor que 0 e maior que 500";
+        assert zn > 0 && zn < 500 : "Zn não pode ser menor que 0 e maior que 500";
+        
         this.codigo = codigo;
         this.nome = nome;
         this.energiaKcal = energiaKcal;
